052437b31aa57980cf3cc49d4a19efe99c4881fc
SilkRoad007 <131393457+SilkRoad007@users.noreply.github.com>
2023-05-12 17:39:25 +0200

    Add files via upload

---
+++ Masonry.desandro.com/1B. Gallery Image 3 Columns + Zoom Image + Button - Responsive/script.js
@@ -0,0 +1,22 @@
+$(function() {
+  // Initialisation de Masonry
+  var $grid = $('.grid').masonry({
+    itemSelector: '.grid-item',
+    columnWidth: '.grid-item',
+    percentPosition: true
+  });
+
+  // Cacher les images après la 9ème
+  $grid.find('.grid-item:gt(8)').hide();
+
+  // Afficher les images suivantes et mettre à jour le layout de Masonry
+  $(document).on('click', '#load-more', function() {
+    $grid.find('.grid-item:hidden:lt(9)').show();
+    $grid.masonry('layout');
+    
+    // Masquer le bouton "Voir plus" si toutes les images ont été chargées
+    if ($grid.find('.grid-item:hidden').length === 0) {
+      $('#load-more').hide();
+    }
+  });
+});
